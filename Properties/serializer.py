9048d2c882c5e0742899fbb909c01db343c29f90
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2024-02-16 12:50:23 +0100

    fixed serializer error

--- Properties/serializer.py
+++ Properties/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Property
 from datetime import date
-class PropertySerializer(serializers.Serializer):
+class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
         fields = '__all__'
@@ -10,5 +10,5 @@ class PropertySerializer(serializers.Serializer):
         data = super().to_representation(instance)
         data['date_listed'] = instance.date_listed
         days_on_site = (date.today() - instance.date_listed).days
-        data['days_on_site'] = days_on_site
+        data['days_on_site'] = days_on_site + 1
         return data
--- agents/serializer.py
+++ agents/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Agent
-class AgentSerializer(serializers.Serializer):
+class AgentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Agent
         fields = '__all__'
